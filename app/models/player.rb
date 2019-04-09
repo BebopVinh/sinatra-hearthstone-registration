9d619e839c9ccead24546b2ef48be3e586228471
BebopVinh <vinhdo07@gmail.com>
2019-04-08 22:54:47 -0700

    added validation to registration

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,16 @@
 class Player < ActiveRecord::Base
+   #bcrypt requirement
    has_secure_password
 
+   #associations
    belongs_to :country
    has_many :player_decks
    has_many :decks, through: :player_decks
+
+   #validation for uniqueness
+   validates :email, confirmation: { case_sensitive: false }, uniqueness: true
+   validates :email_confirmation, presence: true
+   validates :password, confirmation: true
+   validates :password_confirmation, presence: true
+   validates :username, uniqueness: true
 end
