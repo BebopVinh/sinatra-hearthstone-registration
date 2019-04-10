3271786c765b53114b3289ea0d946b285436b41e
BebopVinh <vinhdo07@gmail.com>
2019-04-09 18:03:42 -0700

    players can now see all decks and edit their own

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -3,5 +3,28 @@ class DecksController < ApplicationController
       logged_in?
       erb :'/decks/index'
    end
+
+   get '/decks/:id' do
+      @deck = Deck.find(params[:id])
+      erb :'/decks/show'
+   end
    
+   get '/decks/:id/edit' do
+      logged_in?
+      @deck = Deck.find(params[:id])
+      erb :'/decks/edit'
+   end
+
+   patch '/decks/:id' do
+      @deck = Deck.find(params[:id])
+      binding.pry
+      if params[:deck].values.include?("")
+         redirect back
+      else
+         @deck.update(params[:deck])
+         @deck.code = params[:code] unless params[:code].empty?
+         @deck.save
+         redirect "/players/#{@deck.creator.slug}/decks"
+      end
+   end
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -16,6 +16,13 @@ class PlayersController < ApplicationController
    end
 
    get '/players/:slug/decks' do
+      @player = Player.find_by_slug(params[:slug])
+      @created_decks = Deck.where(creator_id: @player.id)
+      if @player.decks.empty?
+         @other_decks = @player.decks
+      else
+         @other_decks = @player.decks.delete_if {|deck| deck.creator_id == @player.id}
+      end
       erb :'/players/decks'
    end
    
