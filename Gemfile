eb04e1d74041d89bf3eba1970622b14e17d98573
bebopvinh <vinhdo07@gmail.com>
2019-04-14 17:36:13 -0700

    first heroku push

--- Gemfile
+++ Gemfile
@@ -1,12 +1,14 @@
 source 'http://rubygems.org'
 
+
+ruby '2.5.3'
 gem 'sinatra'
 gem 'activerecord', '~> 4.2', '>= 4.2.6', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 gem 'sinatra-flash'
 gem 'rake'
 gem 'require_all'
-gem 'sqlite3', '~>1.3.6'
+gem 'pg', '~> 0.18.4'
 gem 'thin'
 gem 'shotgun'
 gem 'pry'
@@ -19,4 +21,5 @@ group :test do
   gem 'capybara'
   gem 'rack-test'
   gem 'database_cleaner', git: 'https://github.com/bmabey/database_cleaner.git'
+  gem 'sqlite3', '~>1.3.6'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,22 @@ HASHKEY = "894b68302cdfd6479cef84b8388689230d12886afdf02e09ee3605b42dcc68bf"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
 
-# require 'sysrandom/securerandom'
-# require './app/controllers/application_controller'
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+    )
+end
+
 require_all 'app'
